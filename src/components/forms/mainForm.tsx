a36f1d87f150b38cc717bdd9473e954bcf79a3ef
Nielleeeee <jandnllplz@gmail.com>
2025-02-09 01:58:23 +0800

    chore: finished main form

--- src/components/forms/mainForm.tsx
+++ src/components/forms/mainForm.tsx
@@ -7,7 +7,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { toast } from "react-toastify";
 import { sendEmail } from "@/app/action/sendEmail";
 import { FirstStepForm } from "@/components/forms/firstStepForm";
-import { SecondStepForm } from "@/components/forms//secondStepForm";
+import { SecondStepForm } from "@/components/forms/secondStepForm";
+import { ThirdStepForm } from "@/components/forms/thirdStepForm";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -19,12 +20,9 @@ export default function MainForm() {
   });
 
   const {
-    // register,
+    register,
     handleSubmit,
-    // formState: { errors, isSubmitting },
-    // control,
-    // watch,
-    // setValue,
+    formState: { isSubmitting },
     reset,
   } = useForm<MainFormProps>({
     resolver: yupResolver(validationSchema),
@@ -32,47 +30,75 @@ export default function MainForm() {
 
   const onSubmit: SubmitHandler<MainFormProps> = async (data) => {
     try {
+      console.log("Form Data:", data);
+
       const response = sendEmail(data);
 
       await toast.promise(response, {
-        pending: "Sending contact form...",
-        success: "Contact form sent successfully.",
+        pending: "Sending Valentines Invitation...",
+        success: "Valentines Invitation sent successfully.",
         error: "Something went wrong.",
       });
     } catch (error) {
       console.error("An error occurred:", error);
     } finally {
       reset();
+      setActiveForm(0);
     }
   };
 
   const formElements = [
     <FirstStepForm key="firstStep" />,
     <SecondStepForm key="secondStep" />,
+    <ThirdStepForm key="thirdStep" register={register} />,
   ];
+
+  const isLastForm = activeForm === formElements.length - 1;
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
       <div>{formElements[activeForm]}</div>
 
-      {/* next & back button */}
-      <div className="w-full flex flex-row gap-4 items-center justify-center">
-        <button
-          disabled={activeForm === 0}
-          onClick={() => setActiveForm((prev) => prev - 1)}
-          className="bg-gray-500 text-white rounded-lg px-4 py-2"
-        >
-          No
-        </button>
+      {/* back & submit button */}
+      {isLastForm && (
+        <div className="w-full flex flex-row gap-4 items-center justify-center">
+          <button
+            onClick={() => setActiveForm((prev) => prev - 1)}
+            className="bg-gray-500 text-white rounded-lg px-4 py-2"
+          >
+            Back
+          </button>
 
-        <button
-          disabled={activeForm === formElements.length - 1}
-          onClick={() => setActiveForm((prev) => prev + 1)}
-          className="bg-accent text-white rounded-lg px-4 py-2"
-        >
-          Yes
-        </button>
-      </div>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="bg-accent text-white rounded-lg px-4 py-2"
+          >
+            Submit
+          </button>
+        </div>
+      )}
+
+      {/* next & back button */}
+      {!isLastForm && (
+        <div className="w-full flex flex-row gap-4 items-center justify-center">
+          <button
+            disabled={activeForm === 0}
+            onClick={() => setActiveForm((prev) => prev - 1)}
+            className="bg-gray-500 text-white rounded-lg px-4 py-2"
+          >
+            No
+          </button>
+
+          <button
+            disabled={isLastForm}
+            onClick={() => setActiveForm((prev) => prev + 1)}
+            className="bg-accent text-white rounded-lg px-4 py-2"
+          >
+            Yes
+          </button>
+        </div>
+      )}
     </form>
   );
 }
