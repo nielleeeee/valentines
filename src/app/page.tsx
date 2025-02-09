e90812a2e21fae29cc5b4a92b7b8692aa4e0b0aa
Nielleeeee <jandnllplz@gmail.com>
2025-02-10 03:36:34 +0800

    chore: added toast ccontainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import MainForm from "@/components/forms/mainForm";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
+      <ToastContainer position="top-center" />
+
       <div className="max-w-lg animate-float w-full shadow-md bg-primaryWhite gap-10 px-4 py-8 rounded-lg flex flex-col items-center justify-center">
         <MainForm />
       </div>
